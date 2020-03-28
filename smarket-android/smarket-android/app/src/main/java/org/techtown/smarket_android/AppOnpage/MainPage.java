cfe639cc0596e6e73a450ae90cb4fbb77ccbb248
psh <714tkdgns@gmail.com>
2020-03-28 11:15:26 +0900

    feature_search_psh

---
+++ smarket-android/smarket-android/app/src/main/java/org/techtown/smarket_android/AppOnpage/MainPage.java
@@ -0,0 +1,16 @@
+package org.techtown.smarket_android.AppOnpage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.techtown.smarket_android.R;
+
+
+public class MainPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
