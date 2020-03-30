e8c001106d70df360e1e7e0d2b610fbb20c23f34
tomato8160 <tpgus2303@naver.com>
2020-03-31 00:19:41 +0900

    bookmark save2

--- smarket-android/app/src/main/java/org/techtown/smarket_android/User/Bookmark/bookmark_fragment.java
+++ smarket-android/app/src/main/java/org/techtown/smarket_android/User/Bookmark/bookmark_fragment.java
@@ -1,17 +1,24 @@
 package org.techtown.smarket_android.User.Bookmark;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.FocusFinder;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,7 +29,10 @@ import com.android.volley.toolbox.StringRequest;
 
 import org.techtown.smarket_android.R;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class bookmark_fragment extends Fragment {
@@ -36,6 +46,7 @@ public class bookmark_fragment extends Fragment {
     ArrayAdapter spinnerAdapter;
     ViewGroup viewGroup;
     Spinner bookmark_spinner;
+    EditText bookmark_folder_name;
 
     @Nullable
     @Override
@@ -43,6 +54,7 @@ public class bookmark_fragment extends Fragment {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.bookmark_main, container, false);
 
         bookmarkList = new ArrayList<>();
+
         bookmarkList.add("철수");
 
 
@@ -86,8 +98,22 @@ public class bookmark_fragment extends Fragment {
         LayoutInflater inflater = getLayoutInflater();
 
         final View dialogView = inflater.inflate(R.layout.bookmark_plus_dialog, null);
-        final EditText bookmark_folder_name = dialogView.findViewById(R.id.bookmark_folder_name);
+        bookmark_folder_name = dialogView.findViewById(R.id.bookmark_folder_name);
 
+        bookmark_folder_name.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                switch (actionId) {
+                    case EditorInfo.IME_ACTION_NEXT:
+                        // 검색 동작
+                        break;
+                    default:
+                        // 기본 엔터키 동작
+                        return false;
+                }
+                return true;
+            }
+        });
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setView(dialogView);
         builder.setTitle("북마크 폴더 추가");
@@ -97,37 +123,36 @@ public class bookmark_fragment extends Fragment {
                 String folder_name = bookmark_folder_name.getText().toString();
                 bookmarkList.add(folder_name); // 북마크 폴더 추가
                 spinnerAdapter.notifyDataSetChanged(); // 어댑터 갱신
+                bookmark_spinner.setSelection(bookmarkList.size()-1); // 새로운 북마크 생성 시 생성된 북마크 페이지
             }
         });
 
         AlertDialog dialog = builder.create();
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE); // 다이얼로그 생성시 EditText 활성화 1
         dialog.show();
+        if(bookmark_folder_name.requestFocus())
+            ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(bookmark_folder_name, 0); // 다이얼로그 생성시 EditText 활성화 2
 
     }  // 북마크 폴더 추가 기능
 
     private void folder_remove(){
-
-        LayoutInflater inflater = getLayoutInflater();
-        final View dialoView = inflater.inflate(R.layout.bookmark_trashcan_dialog, null);
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setView(dialoView);
-        builder.setTitle("북마크 폴더 삭제");
+        builder.setTitle("북마크 삭제")
+                .setMessage("현재 북마크를 삭제 하시겠습니까?")
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext(), bookmark_spinner.getSelectedItem().toString() + " 북마크가 삭제 되었습니다.", Toast.LENGTH_LONG).show();
+                        spinnerAdapter.remove(bookmark_spinner.getSelectedItem());                    }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-        final String data[] = bookmarkList.toArray(new String[bookmarkList.size()]);
-        final boolean checked[] = new boolean[bookmarkList.size()];
-        
+                    }
+                });
+        builder.create();
+        builder.show();
 
-        builder.setMultiChoiceItems(data, null, new DialogInterface.OnMultiChoiceClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                checked[which] = isChecked;
-            }
-        });
-        builder.setPositiveButton("삭제", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-            }
-        });
-    }
+    }// 북마크 폴더 삭제 기능
 }
